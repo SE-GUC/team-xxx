9cbc5d23583ca0b65fa1d0d354241f4c789fdf16
Ahmed Mohamed Ehab Gaber Elshazly <ahmed.shazly15@gmail.com>
2019-03-02 16:11:20 +0200

    Update Members.js

--- routes/api/Members.js
+++ routes/api/Members.js
@@ -144,16 +144,16 @@ router.get("/skills/:id", function(req, res) {
       returnres.status(200).json(doc.skills);
     })
     .catch(err => next(err));
-
-  // @route   GET api/Members
-  // @desc    Get All Members
-  // @access  Public
-  router.get("/", (req, res) => {
-    Member.find()
-      .sort({ name: 1 })
-      .then(Members => res.json(Members));
-  });
 });
+// @route   GET api/Members
+// @desc    Get All Members
+// @access  Public
+router.get("/", (req, res) => {
+  Member.find()
+    .sort({ name: 1 })
+    .then(Members => res.json(Members));
+});
+
 //@ find a specific Member by ID
 router.get("/:id", function(req, res) {
   Member.findById(req.params.id)
@@ -166,16 +166,16 @@ router.get("/:id", function(req, res) {
     .catch(err => next(err));
 });
 
-  // @route   DELETE api/Members/:id
-  // @desc    Delete A Member
-  // @access  Public
-  router.delete("/:id", (req, res) => {
-    Member.findById(req.params.id)
-      .then(Member => Member.remove().then(() => res.json({ success: true })))
-      .catch(err => res.status(404).json({ success: false })); //console.log("")
-  });
-  router.get("/masterclasses", (req, res) => {
-    /*Member.findOne ({Members: req.body.masterclasses}).then(Members => {
+// @route   DELETE api/Members/:id
+// @desc    Delete A Member
+// @access  Public
+router.delete("/:id", (req, res) => {
+  Member.findById(req.params.id)
+    .then(Member => Member.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false })); //console.log("")
+});
+router.get("/masterclasses", (req, res) => {
+  /*Member.findOne ({Members: req.body.masterclasses}).then(Members => {
     if (!masterclasses){
         errors.nozeft = 'there is no nila';
         return res.status(404).json(errors);
@@ -184,126 +184,126 @@ router.get("/:id", function(req, res) {
 	  })
 	  .catch(err => res.status(404).json(err));
 	*/
-    /*Member.findOne ({Member: req.params.id}).then(masterclasses => {
+  /*Member.findOne ({Member: req.params.id}).then(masterclasses => {
 	  if (masterclasses){*/
-    //update
+  //update
 
-    Member.findOne(
-      { nila: req.params.masterclasses },
+  Member.findOne(
+    { nila: req.params.masterclasses },
 
-      { nila: true }
-    ).catch(err => res.status(404).json(err));
-    /*if (!masterclasses){
+    { nila: true }
+  ).catch(err => res.status(404).json(err));
+  /*if (!masterclasses){
 	      res.json (masterclasses);
 	  }*/
+});
+
+// @route   POST api/Members
+// @desc    Create An Member
+// @access  Public
+router.post("/", (req, res) => {
+  const newMember = new Member({
+    name: req.body.name,
+    age: req.body.age,
+    skills: req.body.skills,
+    interests: req.body.interests,
+    events: req.body.events,
+    tasks: req.body.tasks,
+    reviews: req.body.reviews,
+    masterclasses: req.body.masterclasses,
+    Lifecoach: req.body.Lifecoach,
+    membership: req.body.membership,
+    Contracts: req.body.Contracts,
+    Email: req.body.Email,
+    Password: req.body.Password,
+    Notifications: req.body.Notifications,
+    ReviewOwner: req.body.ReviewOwner,
+    projects: req.body.projects,
+    oldProjects: req.body.oldProjects,
+    RecommendedTasks: req.body.RecommendedTasks
   });
 
-  // @route   POST api/Members
-  // @desc    Create An Member
-  // @access  Public
-  router.post("/", (req, res) => {
-    const newMember = new Member({
-      name: req.body.name,
-      age: req.body.age,
-      skills: req.body.skills,
-      interests: req.body.interests,
-      events: req.body.events,
-      tasks: req.body.tasks,
-      reviews: req.body.reviews,
-      masterclasses: req.body.masterclasses,
-      Lifecoach: req.body.Lifecoach,
-      membership: req.body.membership,
-      Contracts: req.body.Contracts,
-      Email: req.body.Email,
-      Password: req.body.Password,
-      Notifications: req.body.Notifications,
-      ReviewOwner: req.body.ReviewOwner,
-      projects: req.body.projects,
-      oldProjects: req.body.oldProjects,
-      RecommendedTasks: req.body.RecommendedTasks
-    });
+  newMember.save().then(Member => res.json(Member));
+});
 
-    newMember.save().then(Member => res.json(Member));
-  });
-
-  router.put("/update/:id", function(req, res) {
-    var id = req.params.id;
-    Member.findOne({ _id: id }, function(err, foundObject) {
-      if (err) {
-        console.log(err);
+router.put("/update/:id", function(req, res) {
+  var id = req.params.id;
+  Member.findOne({ _id: id }, function(err, foundObject) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (!foundObject) {
+        res.status(404).send();
       } else {
-        if (!foundObject) {
-          res.status(404).send();
-        } else {
-          if (req.body.Email) {
-            foundObject.Email = req.body.Email;
-          }
-          if (req.body.Password) {
-            foundObject.Password = req.body.Password;
-          }
-          if (req.body.name) {
-            foundObject.name = req.body.name;
-          }
-          if (req.body.age) {
-            foundObject.age = req.body.age;
-          }
-          if (req.body.skills) {
-            foundObject.skills = req.body.skills;
-          }
-          if (req.body.interests) {
-            foundObject.interests = req.body.interests;
-          }
-          if (req.body.events) {
-            foundObject.events = req.body.events;
-          }
-          if (req.body.tasks) {
-            foundObject.tasks = req.body.tasks;
-          }
-          if (req.body.reviews) {
-            foundObject.reviews = req.body.reviews;
-          }
-          if (req.body.masterclasses) {
-            foundObject.masterclasses = req.body.masterclasses;
-          }
-          if (req.body.Lifecoach) {
-            foundObject.Lifecoach = req.body.Lifecoach;
-          }
-          if (req.body.membership) {
-            foundObject.membership = req.body.membership;
-          }
-          if (req.body.Contracts) {
-            foundObject.Contracts = req.body.Contracts;
-          }
-          if (req.body.Notifications) {
-            foundObject.Notifications = req.body.Notifications;
-          }
-          if (req.body.RecommendedTasks) {
-            foundObject.RecommendedTasks = req.body.RecommendedTasks;
-          }
-          if (req.body.oldProjects) {
-            foundObject.oldProjects= req.body.oldProjects;
-          }
-          if (req.body.projects) {
-            foundObject.projects = req.body.projects;
-          }
-          foundObject.save(function(err, updatedObject) {
-            if (err) {
-              console.log(err);
-            } else {
-              res.send(updatedObject);
-            }
-          });
+        if (req.body.Email) {
+          foundObject.Email = req.body.Email;
         }
+        if (req.body.Password) {
+          foundObject.Password = req.body.Password;
+        }
+        if (req.body.name) {
+          foundObject.name = req.body.name;
+        }
+        if (req.body.age) {
+          foundObject.age = req.body.age;
+        }
+        if (req.body.skills) {
+          foundObject.skills = req.body.skills;
+        }
+        if (req.body.interests) {
+          foundObject.interests = req.body.interests;
+        }
+        if (req.body.events) {
+          foundObject.events = req.body.events;
+        }
+        if (req.body.tasks) {
+          foundObject.tasks = req.body.tasks;
+        }
+        if (req.body.reviews) {
+          foundObject.reviews = req.body.reviews;
+        }
+        if (req.body.masterclasses) {
+          foundObject.masterclasses = req.body.masterclasses;
+        }
+        if (req.body.Lifecoach) {
+          foundObject.Lifecoach = req.body.Lifecoach;
+        }
+        if (req.body.membership) {
+          foundObject.membership = req.body.membership;
+        }
+        if (req.body.Contracts) {
+          foundObject.Contracts = req.body.Contracts;
+        }
+        if (req.body.Notifications) {
+          foundObject.Notifications = req.body.Notifications;
+        }
+        if (req.body.RecommendedTasks) {
+          foundObject.RecommendedTasks = req.body.RecommendedTasks;
+        }
+        if (req.body.oldProjects) {
+          foundObject.oldProjects = req.body.oldProjects;
+        }
+        if (req.body.projects) {
+          foundObject.projects = req.body.projects;
+        }
+        foundObject.save(function(err, updatedObject) {
+          if (err) {
+            console.log(err);
+          } else {
+            res.send(updatedObject);
+          }
+        });
       }
-    });
+    }
   });
+});
 
-  // @route   DELETE api/Members/:id
-  // @desc    Delete A Member
-  // @access  Public
-  router.delete("/:id", (req, res) => {
-    Member.findById(req.params.id)
-      .then(Member => Member.remove().then(() => res.json({ success: true })))
-      .catch(err => res.status(404).json({ success: false }));
-  });
+// @route   DELETE api/Members/:id
+// @desc    Delete A Member
+// @access  Public
+router.delete("/:id", (req, res) => {
+  Member.findById(req.params.id)
+    .then(Member => Member.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+});
 module.exports = router;
