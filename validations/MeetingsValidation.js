f41b3fd8bcbb35f67586eb9ab69e7e6ad3345326
GeorgesKhelo <georges.khalil@student.guc.edu.eg>
2019-03-19 17:02:53 +0200

    new validations

---
+++ validations/MeetingsValidation.js
@@ -0,0 +1,39 @@
+const Joi = require("joi");
+
+module.exports = {
+  createValidation: request => {
+    const createSchema = {
+        MemberemailOne: Joi.string()
+        .email()
+        .required(),
+        MemberemailTwo: Joi.string()
+        .email()
+        .required(),
+        Location: Joi.string()
+        .required(), 
+        Time: Joi.date()
+        .required(), 
+        StatusMemberOne: Joi.boolean(), 
+        StatusMemberTwo: Joi.boolean()
+    };
+    return Joi.validate(request, createSchema);
+  },
+
+  updateValidation: request => {
+    const updateSchema = {
+        MemberemailOne: Joi.string()
+        .email()
+        .required(),
+        MemberemailTwo: Joi.string()
+        .email()
+        .required(),
+        Location: Joi.string()
+        .required(), 
+        Time: Joi.date()
+        .required(), 
+        StatusMemberOne: Joi.boolean(), 
+        StatusMemberTwo: Joi.boolean()
+    };
+    return Joi.validate(request, updateSchema);
+  }
+};
---
+++ validations/PartnersValidation.js
@@ -0,0 +1,57 @@
+const Joi = require("joi");
+
+module.exports = {
+    createValidation: request => {
+        const createSchema = {
+          Email: Joi.string()
+            .email()
+            .required(),
+          Password: Joi.string()
+            .min(3)
+            .max(15)
+            .required(),
+          business: Joi.string().required(),
+          partners: Joi.string().required(),
+          boardmembers: Joi.string().required(),
+          events: Joi.string().required(), 
+          field: Joi.string().required(), 
+          projects: Joi.string().required(),
+          feedback: Joi.string().required(),
+          Lifecoach: Joi.boolean(),
+          membership: Joi.date(), 
+          Contracts: Joi.string(), 
+          Notifications: Joi.string(), 
+          reviews: Joi.string(),
+          ReviewOwner: Joi.string()
+    
+
+        };
+        return Joi.validate(request, createSchema);
+      },
+
+      updateValidation: request => {
+        const updateSchema = {
+            Email: Joi.string()
+            .email()
+            .required(),
+          Password: Joi.string()
+            .min(3)
+            .max(15)
+            .required(),
+          business: Joi.string().required(),
+          partners: Joi.string().required(),
+          boardmembers: Joi.string().required(),
+          events: Joi.string().required(), 
+          field: Joi.string().required(), 
+          projects: Joi.string().required(),
+          feedback: Joi.string().required(),
+          Lifecoach: Joi.boolean(),
+          membership: Joi.date(), 
+          Contracts: Joi.string(), 
+          Notifications: Joi.string(), 
+          reviews: Joi.string(),
+          ReviewOwner: Joi.string()
+        };
+        return Joi.validate(request, updateSchema);
+      }
+    };
