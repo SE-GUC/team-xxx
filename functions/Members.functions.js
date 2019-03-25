7d15779ba6e29f8455a492155bbf20f6558c120b
Elshazlyyy <ahmed.shazly15@gmail.com>
2019-03-25 23:17:03 +0200

    new test

--- functions/Members.functions.js
+++ functions/Members.functions.js
@@ -1,11 +1,17 @@
 const axios = require("axios");
 axios.defaults.adapter = require("axios/lib/adapters/http");
 const functions = {
-    getmasterclasses: async () => {
+  getmasterclasses: async () => {
     const masterclasses = await axios
       .get("http://localhost:5000/api/Members/")
       .catch(err => "error");
     return masterclasses;
+  },
+  getevents: async () => {
+    const events = await axios
+      .get("http://localhost:5000/api/Members/events/5c90f6d752429b1d4ce3dc11")
+      .catch(err => "error");
+    return events;
   }
 };
 
--- tests/Members.test.js
+++ tests/Members.test.js
@@ -6,3 +6,10 @@ test("6.12 : As a user I should be able to to show my certficates and masterclas
   expect(response.data[1].masterclasses).toEqual("photographee");
   expect(Array.isArray(response.data)).toBe(true);
 });
+
+test("6.1 : As a user I should be able to to show my attended events", async () => {
+  expect.assertions(2);
+  const response = await functions.getevents();
+  expect(response.data).toEqual("beyonds");
+  expect(Array.isArray(response.data)).toBe(false);
+});
