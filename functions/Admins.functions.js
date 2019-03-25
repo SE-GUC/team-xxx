126937849df12bbf636ba72e8005ca9e6fac43d9
Elshazlyyy <ahmed.shazly15@gmail.com>
2019-03-25 09:43:31 +0200

    new test

--- functions/Admins.functions.js
+++ functions/Admins.functions.js
@@ -6,6 +6,11 @@ const functions = {
       .get("http://localhost:5000/api/Admins/5c7a985bc7cac00498980354")
       .catch(err => "error");
     return Admins;
+  }, getDescriptionForAdmin: async () => {
+    const Description = await axios
+      .get("http://localhost:5000/api/Projects/5c7aa93aa8f0f42afbe8fa3b/description")
+      .catch(err => "error");
+    return Description;
   }
 };
 
--- tests/Admins.test.js
+++ tests/Admins.test.js
@@ -7,3 +7,8 @@ test("test", async () => {
   const response = await functions.getAdmins();
   expect(response.data.Email).toEqual("update2@gmail.net");
 });
+test("As an admin I should be able to access the description posted by the partner", async () => {
+    expect.assertions(1);
+    const response = await functions.getDescriptionForAdmin();
+    expect(response.data).toEqual("asd");
+  });
