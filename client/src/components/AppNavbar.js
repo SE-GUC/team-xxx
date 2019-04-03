c71753820463f4924a86e0629323246078af296d
Elshazlyyy <ahmed.shazly15@gmail.com>
2019-04-03 16:30:46 +0200

    new

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -26,7 +26,7 @@ import React, { Component } from 'react';
 	      <div>
 	        <Navbar color="dark" dark expand="sm" className="mb-5">
 	          <Container>
-	            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+	            <NavbarBrand href="/">Lirten Hub</NavbarBrand>
 	            <NavbarToggler onClick={this.toggle} />
 	            <Collapse isOpen={this.state.isOpen} navbar>
 	              <Nav className="ml-auto" navbar>
