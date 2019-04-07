3fab5805abbffbeac360a2b95a9098f4e8bdd50c
Elshazlyyy <ahmed.shazly15@gmail.com>
2019-04-07 16:20:23 +0200

    fix

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,6 @@
-export const GET_PROJECTS = 'GET_PROJECTS';
-export const PROJECTS_LOADING = 'PROJECTS_LOADING';
-export const DELETE_PROJECT = 'DELETE_PROJECT';
-export const ADD_PROJECT = 'ADD_PROJECT';
+export const GET_PROJECTS = "GET_PROJECTS";
+export const PROJECTS_LOADING = "PROJECTS_LOADING";
+export const DELETE_PROJECT = "DELETE_PROJECT";
+export const ADD_PROJECT = "ADD_PROJECT";
+export const ADD_SLOT = "ADD_SLOT";
+export const SLOTS_LOADING = "SLOTS_LOADING";
