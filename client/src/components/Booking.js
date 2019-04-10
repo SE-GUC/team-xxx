c4ae9a158a83d123b99288c89d0ddfcb1e1025c6
Elshazlyyy <ahmed.shazly15@gmail.com>
2019-04-10 23:55:06 +0200

    new

--- client/src/components/Booking.js
+++ client/src/components/Booking.js
@@ -1,4 +1,8 @@
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { getSlots } from "../actions/SlotActions";
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import {
   Card,
   Button,
@@ -14,13 +18,21 @@ import {
 } from "reactstrap";
 
 class Booking extends Component {
+  static propTypes = {
+    getSlots: PropTypes.func.isRequired,
+    Slot: PropTypes.object.isRequired
+  };
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
   NewSlot = () => {
     this.props.history.push("/AddSlot");
   };
+  componentDidMount() {
+    this.props.getSlots();
+  }
   render() {
+    const { Slots } = this.props.Slot;
     return (
       <div>
         <Container>
@@ -36,81 +48,37 @@ class Booking extends Component {
         </Container>
         <Container>
           <CardDeck>
-            <Card body outline color="secondary">
-              <CardTitle>Special Title Treatment</CardTitle>
-              <CardText>
-                With supporting text below as a natural lead-in to additional
-                content.
-              </CardText>
-              <Button>Book</Button>
-              <br />
-              <Button>Confirm</Button>
-              <br />
-              <FormGroup>
-                <Label for="exampleEmail" hidden>
-                  Email
-                </Label>
-                <Input
-                  type="email"
-                  name="email"
-                  id="exampleEmail"
-                  placeholder="Suggest Location"
-                />
-              </FormGroup>
-              <Button>Submit Location</Button>
-              <br />
-              <Button>Confirm</Button>
-            </Card>
-            <Card body outline color="secondary">
-              <CardTitle>Special Title Treatment</CardTitle>
-              <CardText>
-                With supporting text below as a natural lead-in to additional
-                content.
-              </CardText>
-              <Button>Book</Button>
-              <br />
-              <Button>Confirm</Button>
-              <br />
-              <FormGroup>
-                <Label for="exampleEmail" hidden>
-                  Email
-                </Label>
-                <Input
-                  type="email"
-                  name="email"
-                  id="exampleEmail"
-                  placeholder="Suggest Location"
-                />
-              </FormGroup>
-              <Button>Submit Location</Button>
-              <br />
-              <Button>Confirm</Button>
-            </Card>
-            <Card body outline color="secondary">
-              <CardTitle>Special Title Treatment</CardTitle>
-              <CardText>
-                With supporting text below as a natural lead-in to additional
-                content.
-              </CardText>
-              <Button>Book</Button>
-              <br />
-              <Button>Confirm</Button>
-              <br />
-              <FormGroup>
-                <Label for="exampleEmail" hidden>
-                  Email
-                </Label>
-                <Input
-                  type="email"
-                  name="email"
-                  id="exampleEmail"
-                  placeholder="Suggest Location"
-                />
-              </FormGroup>
-              <Button>Submit Location</Button>
-              <br />
-              <Button>Confirm</Button>
-            </Card>
+            <TransitionGroup className="Slots">
+              {Slots.map(({ _id, lifecoachEmail }) => (
+                <CSSTransition key={_id} timeout={500} classNames="fade">
+                  <Card body outline color="secondary">
+                    <CardTitle>{lifecoachEmail}</CardTitle>
+                    <CardText>
+                      With supporting text below as a natural lead-in to
+                      additional content.
+                    </CardText>
+                    <Button>Book</Button>
+                    <br />
+                    <Button>Confirm</Button>
+                    <br />
+                    <FormGroup>
+                      <Label for="exampleEmail" hidden>
+                        Email
+                      </Label>
+                      <Input
+                        type="email"
+                        name="email"
+                        id="exampleEmail"
+                        placeholder="Suggest Location"
+                      />
+                    </FormGroup>
+                    <Button>Submit Location</Button>
+                    <br />
+                    <Button>Confirm</Button>
+                  </Card>
+                </CSSTransition>
+              ))}
+            </TransitionGroup>
           </CardDeck>
         </Container>
       </div>
@@ -118,4 +86,11 @@ class Booking extends Component {
   }
 }
 
-export default Booking;
+const mapStateToProps = state => ({
+  Slot: state.Slot
+});
+
+export default connect(
+  mapStateToProps,
+  { getSlots }
+)(Booking);
